edbb2b2d28bc82b843e7b5022512a49e53f07748
nizarrr16 <mochnaufalnizarafi@gmail.com>
2024-10-11 20:05:16 +0700

    Initial commit

---
+++ Assets/Script/pipecontroller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeController : MonoBehaviour
+{
+    public float pipeSpeed; //mengatur kecepatan pergerakan pipa
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left *  pipeSpeed * Time.deltaTime); //menggeser pipa ke arah kiri
+    }
+}
